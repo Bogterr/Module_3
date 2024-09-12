2cac6b7db64f5f896e170b939a573b64d96ccd69
Bogterr <94174009+Bogterr@users.noreply.github.com>
2024-09-12 14:23:04 +0700

    Add files via upload

---
+++ Hard_task.py
@@ -0,0 +1,25 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+def calculate_structure_sum(*args):
+    sum = 0
+    for arg in args:
+        if isinstance(arg, (int, float)):
+            sum += arg
+        elif isinstance(arg, str):
+            sum += len(arg)
+        elif isinstance(arg, (list, tuple, set)):
+            sum += calculate_structure_sum(*arg)
+        elif isinstance(arg, dict):
+            sum += calculate_structure_sum(*arg.items())
+    return sum
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
